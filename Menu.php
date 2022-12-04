978a5f5ed969d9188f7500518e623a54d4e9bcc8
Simon <s4siszul@uni-trier.de>
2022-12-04 14:07:33 +0100

    Verlinkung repariert

--- Menu.php
+++ Menu.php
@@ -9,10 +9,10 @@
 </style>
 
 <ul class="list-group">
-    <li class="list-group-item sidebarHauptelement"><a href="login.php" class="text-decoration-none">Login</a></li>
-    <li class="list-group-item sidebarHauptelement"><a href="projekte.php" class="text-decoration-none">Projekte</a></li>
+    <li class="list-group-item sidebarHauptelement"><a href="Login.php" class="text-decoration-none">Login</a></li>
+    <li class="list-group-item sidebarHauptelement"><a href="Projekte.php" class="text-decoration-none">Projekte</a></li>
     <li class="list-group-item sidebarHauptelement"><a href="ToDoList.php" class="text-decoration-none">Aktuelles Projekt</a></li>
-    <li class="list-group-item ms-4 sidebarUnterelement"><a href="reiter.php" class="text-decoration-none">Reiter</a></li>
-    <li class="list-group-item ms-4 sidebarUnterelement"><a href="aufgaben.php" class="text-decoration-none">Aufgaben</a></li>
-    <li class="list-group-item ms-4 sidebarUnterelement"><a href="personen.php" class="text-decoration-none">Mitglieder</a></li>
+    <li class="list-group-item ms-4 sidebarUnterelement"><a href="Reiter.php" class="text-decoration-none">Reiter</a></li>
+    <li class="list-group-item ms-4 sidebarUnterelement"><a href="Aufgaben.php" class="text-decoration-none">Aufgaben</a></li>
+    <li class="list-group-item ms-4 sidebarUnterelement"><a href="Personen.php" class="text-decoration-none">Mitglieder</a></li>
 </ul>
