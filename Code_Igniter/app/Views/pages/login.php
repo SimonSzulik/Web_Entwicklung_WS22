0233ea448e0c4a9cd15a7ae548f3ae052b7b40db
Simon <s4siszul@uni-trier.de>
2022-12-16 18:15:14 +0100

    Added CodeIgnite to the Projekt and refactored Login Page

---
+++ Code_Igniter/app/Views/pages/login.php
@@ -0,0 +1,43 @@
+<!-- Oberster Div Container mit Text -->
+<div class="container-fluid">
+    <div class="text-center bg-light mt-3 mb-3 p-5">
+        <h1>Aufgabenplaner: Login</h1>
+    </div>
+    <!-- Oberster Row Container -->
+    <div class="row pt-3">
+        <!-- Col -->
+        <div class="col-lg-3 p-3"></div>
+        <div class="col">
+            <!-- Row -->
+            <div class="row">
+                <!-- Login -->
+                <div class="col p-3">
+                    <form>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">E-Mail Adresse</label>
+                            <input type="email" class="form-control" id="email" name="email" tabindex="1" placeholder="E-Mail Adresse eingeben">
+                        </div>
+                        <div class="mb-3">
+                            <label for="passwd" class="form-label">Passwort</label>
+                            <input type="password" class="form-control" id="passwd" name="passwd" tabindex="2" placeholder="Passwort">
+                        </div>
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="agb" name="agb">
+                            <label class="form-check-label" for="agb">AGBs und Datenschutzbedingung akzeptieren</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary" id="loginSubmit" name="loginSubmit">Einloggen</button>
+                    </form>
+                    <p>
+                        Noch nicht registriert? <a href="" style="text-decoration: none">Registrierung</a>
+                    </p>
+                   <!-- <p>
+                        Da der Login Vorgang technisch noch nicht realisiert wurde: <a href="ToDoList.php" style="text-decoration: none">Zur ToDoListe</a>
+                    </p>
+                    <p>
+                        Hier geht es zur Ausgabe der Datenbankabfragen: <a href="Datenbankabfragen.php" style="text-decoration: none">klick mich</a>
+                    </p>-->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
